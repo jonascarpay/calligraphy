089f28839911bfe5584269cca8ec0d5339080e42
Hécate Moonlight <hecate+github@glitchbra.in>
2023-03-13 18:00:40 +0100

    Create custom Prelude
    This commit bootstraps the custom Prelude, which will then be adapted to
    re-export base/ghc version-specific imports.

--- test/Test/LexTree.hs
+++ test/Test/LexTree.hs
@@ -6,7 +6,6 @@
 module Test.LexTree where
 
 import Calligraphy.Util.LexTree
-import Control.Applicative
 import qualified Data.Foldable as Foldable
 import Data.Maybe (fromMaybe, isJust)
 import Test.Hspec
