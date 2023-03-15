9a6ddbd9489f82d483e35e6f49d54b97897ad228
Jonas Carpay <jonascarpay@gmail.com>
2023-03-15 15:19:18 +0900

    WIP Mermaid styling

--- src/Calligraphy/Phases/Render/Mermaid.hs
+++ src/Calligraphy/Phases/Render/Mermaid.hs
@@ -7,6 +7,7 @@ where
 
 import Calligraphy.Phases.Render.Common
 import Calligraphy.Util.Printer
+import Calligraphy.Util.Types
 import Control.Monad (forM_)
 import Data.List (intercalate)
 import Data.Tree (Tree (..))
@@ -24,13 +25,30 @@ renderMermaid (RenderGraph roots calls types) = do
       strLn $ caller <> " -.-> " <> callee
   where
     printTree :: Prints (Tree RenderNode)
-    printTree (Node (RenderNode nodeid _ lbll _) []) =
-      strLn $ nodeid <> "[" <> intercalate "\n" lbll <> "]"
-    printTree (Node (RenderNode nodeid _ lbll _) children) =
-      brack ("subgraph " <> nodeid <> " [" <> intercalate "\n" lbll <> "]") "end" $
+    printTree (Node (RenderNode nodeid typ lbll export) []) = do
+      strLn $ nodeid <> nodeShape typ (intercalate "\n" lbll)
+      if export
+        then strLn $ "style " <> nodeid <> " stroke:#777,fill-opacity:0"
+        else strLn $ "style " <> nodeid <> " stroke:#777,fill-opacity:0,stroke-dasharray: 5 5"
+    printTree (Node (RenderNode nodeid typ lbll export) children) = do
+      brack ("subgraph " <> nodeid <> "[" <> intercalate "\n" lbll <> "]") "end" $ do
+        if export
+          then strLn $ "style " <> nodeid <> " stoke:#777,fill-opacity:0"
+          else strLn $ "style " <> nodeid <> " stoke:#777,fill-opacity:0,stroke-dasharray: 5 5"
         forM_ children printTree
+    -- strLn $ nodeid <> " ~~~ " <> nodeId childNode
 
     printModule :: Prints RenderModule
     printModule (RenderModule lbl modId decls) =
       brack ("subgraph " <> modId <> " [" <> lbl <> "]") "end" $
         forM_ decls printTree
+
+nodeShape :: DeclType -> String -> String
+nodeShape DataDecl = wrapBracket "([" "])"
+nodeShape ValueDecl = wrapBracket "[" "]"
+nodeShape RecDecl = wrapBracket "(" ")"
+nodeShape ConDecl = wrapBracket "(" ")"
+nodeShape ClassDecl = wrapBracket "[/" "\\]"
+
+wrapBracket :: String -> String -> String -> String
+wrapBracket p q inner = p <> inner <> q
